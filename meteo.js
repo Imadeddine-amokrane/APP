2b8ca1afb9217ebb2c0d6ba92170afa14628051b
Imadeddine-amokrane <78732796+Imadeddine-amokrane@users.noreply.github.com>
2021-02-10 14:55:09 +0100

    Final

--- meteo.js
+++ meteo.js
@@ -1,2 +1,19 @@
-let villecourante="Paris"
-function showCity() { alert(villeCourante); }
+function showCity() {
+		 
+
+		  let nomVilleSelectionnee = document.getElementById("choisirville" ).value;
+		  
+		  let cities = document.getElementsByClassName("city");
+		  let l=cities.length;
+
+		  for (i=0; i<l; i++) {
+    			if (cities[i].id == nomVilleSelectionnee) {
+    				  
+    				  cities[i].style.display= "";
+   				 } else {
+      				
+      				cities[i].style.display= "none";
+    				}
+  				}
+		  
+}
